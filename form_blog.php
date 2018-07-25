dbac013604e7dade0535d5b3fbe66fcd4c752eb4
Davi Ferreira <davi.ferreira@bomcontrole.com.br>
2018-07-25 18:28:27 -0300

    Formulario de Integração com a RD Station

---
+++ form_blog.php
@@ -0,0 +1,13 @@
+<div class="form">
+
+<div class="col-md-6" style="background: #f8f8f8;"><img src="https://milvus.com.br/wordpress/wp-content/uploads/2018/07/capa-governaca.jpg" style="margin: 15px 2px; margin: 15px 2px;width: 271px;"></div>
+<div class="col-md-6" style="background: #f8f8f8;">
+<div id="e-book-blog-883f30f363d757daf017" role="main">
+</div>
+<script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
+<script type="text/javascript">
+  new RDStationForms('e-book-blog-883f30f363d757daf017-html', 'UA-85965285-1').createForm();
+</script>
+
+</div>
+</div>
